0a5cad46df1890ae2a3d0622c4595de0b639ed77
Samuel <samuel.ademola43@gmail.com>
2020-05-15 23:46:03 +0100

    added in screenshot functionality

--- src/js/app.js
+++ src/js/app.js
@@ -34,6 +34,7 @@ var app = new Vue({
     // currentURL: "https://www.bing.com/",
     currentURL: null,
     show: false,
+    images: []
 
   },
   watch: {
@@ -48,6 +49,67 @@ var app = new Vue({
       } else this.show = false
     }
   },
-  methods: {},
+  methods: {
+    // var newDiv = document.createElement("img");
+
+    screenshot() {
+
+      // navigator.mediaDevices.getUserMedia({
+      navigator.mediaDevices.getDisplayMedia({
+          video: true
+        })
+        .then(mediaStream => {
+          this.track = mediaStream.getVideoTracks()[0];
+
+          this.imageCapture = new ImageCapture(this.track);
+          // this.imageCapture.takePhoto().then(this.processPhoto).catch(console.log("error")); //takes photo
+          // this.grabFrameFunc();
+          this.imageCapture.grabFrame()
+            .then(imageBitmap => {
+              this.images.push(imageBitmap)
+
+              setTimeout(()=>{
+
+                const canvas = this.$refs.canvas;
+                console.log(canvas)
+
+                this.drawCanvas(canvas, imageBitmap);
+              }, 1000)
+            })
+            .catch(error => console.log(error));
+            });
+    },
+    grabFrameFunc() {
+      this.imageCapture.grabFrame().then((bitmap) => {
+        let canvas = document.createElement('canvas');
+        let context = canvas.getContext('2d');
+        context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height)
+        // context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height)
+        return canvas.toDataURL();
+      }).then((src) => {
+        this.images.push(src)
+        console.log(src)
+      }).catch(console.error)
+      console.log(new ImageCapture(track))
+        .catch(error => console.log(error));
+    },
+    drawCanvas(canvas, img) {
+      canvas.width = getComputedStyle(canvas).width.split('px')[0];
+      canvas.height = getComputedStyle(canvas).height.split('px')[0];
+      let ratio = Math.min(canvas.width / img.width, canvas.height / img.height);
+      let x = (canvas.width - img.width * ratio) / 2;
+      let y = (canvas.height - img.height * ratio) / 2;
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+      canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,
+        x, y, img.width * ratio, img.height * ratio);
+    },
+    processPhoto(blob) { //gets blob data from camera and turns it into a Base 64 buffer
+      this.images.push(URL.createObjectURL(blob));
+      console.log(blob)
+      // this.blobToBase64(blob, (base64) => {
+      //   var buf = new Buffer.from(base64, 'base64');
+      // })
+    },
+  },
   mounted() {}
 });
